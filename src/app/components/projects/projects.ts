893c62d64001307cd07126ec5fb16f36186135b4
sobiahub <sobiatech5@gmail.com>
2026-08-06 15:34:10 +0500

    project 1 added

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,4 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+export interface Project {
+
+  title: string;
+
+  category: string;
+
+  description: string;
+
+  technologies: string[];
+
+  live: string;
+
+  github: string;
+
+  images: string[];
+
+}
 
 @Component({
   selector: 'app-projects',
@@ -6,4 +24,84 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent implements OnInit, OnDestroy {
+   currentImage = 0;
+
+   intervalId: any;
+
+  ngOnInit() {
+
+  this.startSlider();
+
+}
+
+ ngOnDestroy() {
+
+  this.stopSlider();
+
+}
+
+
+  startSlider() {
+
+  this.stopSlider();
+
+  this.intervalId = setInterval(() => {
+
+    this.nextImage();
+
+  }, 3000);
+
+}
+
+stopSlider() {
+
+  if (this.intervalId) {
+
+    clearInterval(this.intervalId);
+
+  }
+
+}
+
+  ecoHomeImages = [
+
+  '/images/hero.PNG',
+
+  '/images/categories.PNG',
+
+  '/images/featured-collections.PNG',
+
+  '/images/wishlist.PNG',
+
+  '/images/cart.PNG',
+
+  '/images/newsletter.PNG',
+
+  '/images/login.PNG',
+
+  '/images/signup.PNG'
+
+];
+
+  nextImage() {
+
+  this.currentImage =
+    (this.currentImage + 1) %
+    this.ecoHomeImages.length;
+
+  this.startSlider();
+
+}
+
+ previousImage() {
+
+  this.currentImage =
+    (this.currentImage - 1 + this.ecoHomeImages.length) %
+    this.ecoHomeImages.length;
+
+  this.startSlider();
+
+}
+
+}
